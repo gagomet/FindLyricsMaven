d632cdbc24d75995ea845e6dc46c08bf3ccd7722
gagomet <kir_one@list.ru>
2014-08-11 08:25:53 +0300

    Deleting commented methods

--- src/main/java/com/findlyrics/rest/model/SongPojo.java
+++ src/main/java/com/findlyrics/rest/model/SongPojo.java
@@ -53,15 +53,7 @@ public class SongPojo {
         this.artist = artist;
     }
 
-   /* @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
 
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
-    }*/
 
 }
 
